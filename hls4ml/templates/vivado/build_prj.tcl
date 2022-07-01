0839cb6c5561aaefb9db772dfc0dca454041d692
Javier Duarte <jduarte@ucsd.edu>
2022-07-01 12:58:13 -0700

    Update build_prj.tcl

--- hls4ml/templates/vivado/build_prj.tcl
+++ hls4ml/templates/vivado/build_prj.tcl
@@ -161,7 +161,7 @@ if {$opt(reset)} {
 } else {
     open_solution "solution1"
 }
-catch {config_array_partition -maximum_size 4096}
+catch {config_array_partition -maximum_size 8192}
 config_compile -name_max_length 60
 set_part $part
 create_clock -period $clock_period -name default
